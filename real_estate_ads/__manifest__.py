e5490014bd5a5dcbb07ed822a43d28e9d1e89474
mariem88 <maryemgharssalla@gmail.com>
2025-02-14 13:47:40 +0100

    Ton message de commit

--- real_estate_ads/__manifest__.py
+++ real_estate_ads/__manifest__.py
@@ -1,18 +1,18 @@
 {
-    "name":"real estate ads",
-    "description":"""
-    real estate module to show available properties
+    'name':'Real Estate Ads',
+    'description':"""
+    Real Estate module to show available properties
     """,
-    'version': '18.0.1.0.0',
-    "category":"sales",
-    "depends":[],
-    "data":[
+    'version':'1.0',
+    'category':"Sales",
+    'depends':[],
+    'data':[
         'security/ir.model.access.csv',
         'views/property_view.xml',
-        'views/manu_items.xml',
-
+        'views/menu_items.xml'
     ],
-    "installable":True,
-    "application":True,
-    "license":"LGPL-3"
+
+    'installable':True,
+    'application':True,
+    'license':'LGPL-3'
 }
--- real_estate_ads/models/property.py
+++ real_estate_ads/models/property.py
@@ -1,22 +1,31 @@
 from odoo import fields, models
 
 class Property(models.Model):
-    _name="estate.property"
-    _description="real estate property"
 
-    name=fields.Char(string="Name")
-    description =fields.Text(string="Description")
-    postcode = fields.Char(string="Postcode")
-    date_availability=fields.Date(string="Available From")
-    expected_price=fields.Float(string="Expected Price")
-    best_offer=fields.Float(string="Best Offer")
-    selling_price=fields.Float(string="Selling price")
-    bedrooms=fields.Integer(string="Bedrooms")
-    living_area=fields.Integer(string="Living Area (sqn)")
-    facades=fields.Integer(string="Facades (sqn)")
-    garage =fields.Boolean(string="Garage", default=False)
-    garden=fields.Boolean(string="Garden", default=False)
-    garden_area=fields.Integer(string="Garden Area")
-    garden_orientation=fields.Selection([('north','North'),('south','South'),('east','East'),('west','west')],string="Garden Orientation",default="north")
+    _name = 'estate.property'
+    _description = "Real Estate Property"
 
-    """id,create_date,create_uid,write_date,write_uid"""
+    name = fields.Char(string = "Name" , required = True)
+    description = fields.Text(string = "Description")
+    postcode = fields.Char(string = "Postcode")
+    date_availability = fields.Date(string = "Available From")
+    expected_price = fields.Float(string = "Expected price")
+    best_offer = fields.Float(string = "Best Offer")
+    selling_price = fields.Float(string = "Selling Price")
+    bedrooms = fields.Integer(string = "Bedrooms")
+    living_area = fields.Integer(string = "living Area(sqn)")
+    facades = fields.Integer(string = "Facades(sqn)")
+    garage = fields.Boolean(string = "Garage", default = False)
+    garden = fields.Boolean(string = "Garden", default = False)
+    garden_area = fields.Integer(string = "Garden Area")
+    garden_orientation = fields.Selection(
+        [('north' , 'North') , ('south' , 'South'), ('east' , 'East') , ('west' , 'West')], string = "Garden Orientation" , default = "north")
+
+    """id, create_date, create_uid, write_date, write_uid"""
+
+
+
+
+
+
+

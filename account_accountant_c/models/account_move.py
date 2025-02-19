f58788c5a3ee2b3a530790ecf8940433e6bc9eb3
mariem88 <maryemgharssalla@gmail.com>
2025-02-19 17:03:53 +0100

    Customers/credit notes

--- account_accountant_c/models/account_move.py
+++ account_accountant_c/models/account_move.py
@@ -4,3 +4,12 @@ class AccountMove(models.Model):
     _inherit = 'account.move'  # Héritage du modèle existant
 
     custom_field = fields.Char(string="Champ Personnalisé", help="Ceci est un champ personnalisé pour les écritures comptables.")
+    partner_id = fields.Many2one('res.partner', string='Customer', ondelete='cascade', index=True, store=True)
+
+    # Filtrer uniquement les notes de crédit et les factures clients
+    def _get_credit_notes(self):
+        return self.search([('move_type', 'in', ['out_invoice', 'out_refund'])])
+
+    # Lier la note de crédit aux partenaires clients
+    def get_credit_notes_by_partner(self):
+        return self.search([('move_type', '=', 'out_refund')])  # Note de crédit (out_refund)

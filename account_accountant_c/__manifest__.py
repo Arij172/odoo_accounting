4cee7a3e76f66529a36d7ea19a23a0f763ea808b
arij172 <guizaniarij17@gmail.com>
2025-02-19 13:35:15 +0100

    Customers

--- account_accountant_c/__manifest__.py
+++ account_accountant_c/__manifest__.py
@@ -2,26 +2,21 @@
     'name': 'Custom Accounting',
     'version': '1.0',
     'category': 'Accounting',
-    'summary': 'Invoices, Payments, Follow-ups & Bank synchronization ',
-
+    'summary': 'Invoices, Payments, Follow-ups & Bank synchronization',
     'description': """
-Invoicing Access Rights
-========================
-It gives the Administrator user access to important invoicing features such as bank recon and payment follow-up.
-
-""",
-
-    'depends': ['account', 'base'],
+        Invoicing Access Rights
+        ========================
+        It gives the Administrator user access to important invoicing features such as bank recon and payment follow-up.
+    """,
+    'depends': ['account', 'base',],
     'data': [
         'security/ir.model.access.csv',  # Fichier de sécurité
         'views/account_move_view.xml',  # Vues pour les écritures comptables
-
-        'views/menu_views.xml',
-
+        'views/partner_views.xml',       # Vues pour les clients/fournisseurs
+        'views/menu_views.xml'         # Menuitems
     ],
     'installable': True,
     'auto_install': True,
-    "application":True,
+    'application': True,
     'license': 'LGPL-3',
-
 }

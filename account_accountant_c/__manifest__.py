4a3eb928bd016e86094dc784f0eb719a6b1f0d5a
arij172 <guizaniarij17@gmail.com>
2025-02-19 14:17:21 +0100

    Customers2

--- account_accountant_c/__manifest__.py
+++ account_accountant_c/__manifest__.py
@@ -13,13 +13,10 @@
         'security/ir.model.access.csv',  # Fichier de sécurité
         'views/account_move_view.xml',  # Vues pour les écritures comptables
         'views/partner_views.xml',       # Vues pour les clients/fournisseurs
-        'views/res_partner_views.xml', # View of customers/payments
-        'views/menu_views.xml' # Menuitems
-
+        'views/menu_views.xml'         # Menuitems
     ],
     'installable': True,
     'auto_install': True,
     'application': True,
-
     'license': 'LGPL-3',
 }
